e6dc27b52bb91508277fcb8bfd8879c1868d67b2
MukPan <k22005kk@aitech.ac.jp>
2023-09-08 09:00:40 +0900

    add: 商品削除の確認ダイアログを追加

--- lib/component/item_counter.dart
+++ lib/component/item_counter.dart
@@ -1,3 +1,4 @@
+import 'package:cash_register_app/dialog/remove_item_alert_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../provider/item_count_family.dart';
@@ -23,22 +24,34 @@ class ItemCounter extends HookConsumerWidget {
   final int count = ref.watch(itemCountFamily(index)); //読取
 
     return Container(
+      width: 100,
       margin: const EdgeInsets.only(top: 10, bottom: 10),
       decoration: BoxDecoration(
         color: const Color(0x10000000),
-        borderRadius: BorderRadius.circular(15),
+        borderRadius: BorderRadius.circular(20),
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           IconButton(
-              onPressed: (count <= 0) ? null : () {
-                _decreaseCount(ref);
+              onPressed: (count <= 0) ? null : () async {
+                //カウントが1のとき
+                bool isRemove = false;
+                if (count == 1) {
+                  isRemove = await showDialog(
+                    context: context,
+                    builder: (content) => RemoveItemAlertDialog(index: index)
+                  );
+                }
+
+                if (count > 1 || isRemove) _decreaseCount(ref);
+
               },
-              icon: const Icon(Icons.remove)
+              icon: (count <= 1) ? const Icon(Icons.delete) : const Icon(Icons.remove)
           ),
           Text(count.toString()),
           IconButton(
-              onPressed: () {
+              onPressed: (count >= 5) ? null : () { //上限を設ける
                 _increaseCount(ref);
               },
               icon: const Icon(Icons.add)
