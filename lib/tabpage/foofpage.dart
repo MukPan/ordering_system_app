48d0c07996c55db2839dc1422f2094af16e24962
MukPan <k22005kk@aitech.ac.jp>
2023-10-02 13:07:19 +0900

    add: メニューのグリッドの間隔を調整

--- lib/tabpage/foofpage.dart
+++ lib/tabpage/foofpage.dart
@@ -23,36 +23,39 @@ class Food_page extends StatelessWidget {
 
       body:
       Container(
-        margin: const EdgeInsets.all(30), //外周の余白
+        margin: const EdgeInsets.all(20), //外周の余白
         child: GridView.builder(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisSpacing: 30,     //ボックス左右間のスペース
-            mainAxisSpacing: 30,      //ボックス上下間のスペース
+            crossAxisSpacing: 10,     //ボックス左右間のスペース
+            mainAxisSpacing: 10,      //ボックス上下間のスペース
             crossAxisCount: 2,        //ボックスを横に並べる数
           ),
           itemCount: 10, //要素数
           //指定した要素の数分を生成
           itemBuilder: (context, index) {
-            return ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  elevation: 3, //影の大きさ
-                  foregroundColor: Colors.grey.withAlpha(10), //ボタンを押下時のエフェクト色と文字色(子要素で上書き可能)
-                  backgroundColor: Colors.white,
-                  surfaceTintColor: Colors.white,
-                  shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(0)) //角ばったボタン、数値を上げると丸くなる
+            return Container(
+              margin: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 3, //影の大きさ
+                    foregroundColor: Colors.grey.withAlpha(10), //ボタンを押下時のエフェクト色と文字色(子要素で上書き可能)
+                    backgroundColor: Colors.white,
+                    surfaceTintColor: Colors.white,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(0)) //角ばったボタン、数値を上げると丸くなる
+                    ),
                   ),
-                ),
-                onPressed: () {
-                  showCustomDialog(context);
-                }, //押下時ポップアップ
-                child: const Text( //buttonの中身、商品名や画像、値段など
-                  "button",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                  ),
-                )
+                  onPressed: () {
+                    showCustomDialog(context);
+                  }, //押下時ポップアップ
+                  child: const Text( //buttonの中身、商品名や画像、値段など
+                    "button",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
+                  )
+              ),
             );
           },
         ),
