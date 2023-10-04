1c69278286e8474d45c9c885a39243a1045b14d2
nishi240masa <masa2401106@gmail.com>
2023-10-05 05:11:02 +0900

    fix:ダイアログの小計数のバグ修正

--- lib/dialog/edit_order_dialog.dart
+++ lib/dialog/edit_order_dialog.dart
@@ -47,15 +47,25 @@ void showEditOrderDialog({ //itemInfo, context, ref
 
   final int itemPrice = iteminfo.itemPrice;
 
-  final int optsPrice = (!iteminfo.optInfoList.isNotEmpty) //!を入れることでオプションの有無しを正確な値にしている
+  //時間なくて条件分岐でゴリ押しました。すいません
+  if (iteminfo.category == "drink"){
+    final int optsPrice = (iteminfo.optInfoList.isNotEmpty) //ドリンクはオプションが無いので!を消す
+
+        ? iteminfo.optInfoList
+        .map((optinfo) => optinfo.optPrice)
+        .reduce((sum, price) => sum + price)
+        : 0;
+    final int amountPerItem = itemPrice + optsPrice;
+    ref.read(amountPerItemProvider.notifier).state = amountPerItem;
+
+  }else {final int optsPrice = (!iteminfo.optInfoList.isNotEmpty) //!を入れることでオプションの有無しを正確な値にしている
       ? iteminfo.optInfoList
       .map((optinfo) => optinfo.optPrice)
       .reduce((sum, price) => sum + price)
       : 0;
   final int amountPerItem = itemPrice + optsPrice;
-
-
   ref.read(amountPerItemProvider.notifier).state = amountPerItem;
+  }
 
   //画面サイズ取得
   final screenWidth = MediaQuery.of(context).size.width;
