ff6a7f19d7f2b5472fdd5703c83e5b71f766e0e1
nishi240masa <masa2401106@gmail.com>
2023-10-06 22:10:17 +0900

    add:showDialogのUIを最適化

--- lib/showDialog/showDialog.dart
+++ lib/showDialog/showDialog.dart
@@ -1,18 +1,16 @@
 import 'package:cash_register_app/provider/cart_provider.dart';
-import 'package:cash_register_app/showDialog/sub_total.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../cart/cart.dart';
 import '../database/item_infos.dart';
 import '../image/image_asset.dart';
 import '../provider/amount_per_item.dart';
 import '../provider/countprovider.dart';
 import '../provider/opt_is_enabled.dart';
 import 'bottom.dart';
-import 'count.dart';
-
-import '../cart/cart.dart';
 import 'dialog_buttom.dart';
 
 void showCustomDialog(BuildContext context, WidgetRef ref, ItemInfo iteminfo) {
@@ -61,10 +59,6 @@ void showCustomDialog(BuildContext context, WidgetRef ref, ItemInfo iteminfo) {
   showDialog(
     context: context,
     builder: (BuildContext context) {
-      final String itemName = iteminfo.itemName;
-
-      print(itemName);
-
       return AlertDialog(
         backgroundColor: Colors.white,
         surfaceTintColor: Colors.white,
@@ -75,97 +69,101 @@ void showCustomDialog(BuildContext context, WidgetRef ref, ItemInfo iteminfo) {
           fontSize: 20.sp,
         ),
         titlePadding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
-        contentPadding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
+        contentPadding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
         shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(10.0))),
         //商品名
-        content: SingleChildScrollView(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  //商品名
-                  Text(
-                    itemName,
-                    style: TextStyle(fontSize: 20.sp),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //商品名
+                Text(
+                  itemName,
+                  style: TextStyle(fontSize: 20.sp),
+                ),
+                //バツボタン
+                Container(
+                  margin: const EdgeInsets.only(top: 5),
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(CupertinoIcons.xmark),
                   ),
-                  //バツボタン
-                  Container(
-                    margin: const EdgeInsets.only(top: 5),
-                    child: IconButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      icon: const Icon(CupertinoIcons.xmark),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                width: (screenWidth * 0.7).w, //7割のサイズ
-                height: (iteminfo.optInfoList.isNotEmpty)
-                    ? (screenHeight * 0.75).h //7.5割のサイズ
-                    : null,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 20),
-                      width: double.infinity,
-                      padding: const EdgeInsets.all(15),
-                      // padding: EdgeInsets.,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: ItemImg(itemName: itemName, size: 120),
-                    ),
-                    //見出し
-                    (iteminfo.optInfoList.isNotEmpty)
-                        ? Container(
+                )
+              ],
+            ),
+            SizedBox(
+              width: screenWidth.w, //7割のサイズ
+              height: (screenHeight * 0.6).h,
+              child: Scrollbar(
+                thumbVisibility: true,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 20),
                             width: double.infinity,
-                            margin: const EdgeInsets.symmetric(vertical: 15),
+                            padding: const EdgeInsets.all(15),
+                            // padding: EdgeInsets.,
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: ItemImg(itemName: itemName, size: 120),
+                          ),
+                          //見出し
+                          (iteminfo.optInfoList.isNotEmpty)
+                              ? Container(
+                            width: double.infinity,
+                            margin:
+                            const EdgeInsets.symmetric(vertical: 15),
                             child: Text(
                               'トッピング',
                               style: TextStyle(
                                   fontSize: 20.sp, color: Colors.grey),
                             ),
                           )
-                        : Container(),
-                    //オプション一覧 iteminfo.optInfoList.length
-                    (iteminfo.optInfoList.isNotEmpty)
-                        ? Expanded(
-                            child: ListView.separated(
-                              shrinkWrap: true,
-                              itemCount: iteminfo.optInfoList.length,
-                              separatorBuilder: (context, index) =>
-                                  const Divider(),
-                              itemBuilder: (BuildContext context, int index) {
-                                return OptionTile(
-                                    optInfo: iteminfo.optInfoList[index]);
-                              },
-                            ),
+                              : Container(),
+                          //オプション一覧 iteminfo.optInfoList.length
+                          (iteminfo.optInfoList.isNotEmpty)
+                              ? ListView.separated(
+                            shrinkWrap: true,
+                            itemCount: iteminfo.optInfoList.length,
+                            separatorBuilder: (context, index) =>
+                            const Divider(),
+                            itemBuilder: (BuildContext context, int index) {
+                              return OptionTile(
+                                  optInfo: iteminfo.optInfoList[index]);
+                            },
                           )
-                        : const SizedBox(height: 10),
-                    const Divider(
-                      color: Colors.black,
-                    ), //インデント?
-                 BottomAppBarWidget(
-                        onPressed: () {
-                          final cartListController =
-                              ref.watch(cartListProvider);
-                          final cartService =
-                              CartListService(cartListController);
-                          cartService.addItemToCart(context, ref, iteminfo);
-                          Navigator.of(context).pop();
-                        },
+                              : const SizedBox(height: 10),
+                          const Divider(
+                            color: Colors.black,
+                          ), //インデント?
+                        ],
                       ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ],
-          ),
+
+            ),
+            BottomAppBarWidget(
+              onPressed: () {
+                final cartListController = ref.watch(cartListProvider);
+                final cartService = CartListService(cartListController);
+                cartService.addItemToCart(context, ref, iteminfo);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
         ),
       );
     },
